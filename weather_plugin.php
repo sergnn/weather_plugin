30e1cab4614e394002b25d36424ca5349792d129
moocher <sergiusnn@gmail.com>
2013-02-21 20:55:41 +0400

    Someembellishments

--- weather_plugin.php
+++ weather_plugin.php
@@ -1,5 +1,6 @@
 <?
-function get_direction($rumb){
+function get_direction($rumb)
+{
 	if ($rumb>340) return('С');
 	elseif ($rumb>325) return('ССЗ');
 	elseif ($rumb>305) return('СЗ');
@@ -18,13 +19,10 @@ function get_direction($rumb){
 	elseif ($rumb>20)  return('ССВ');
 	elseif ($rumb>=0)   return('С');
 }
-$dayow = array('вс','пн','вт','ср','чт','пт','сб');
-$month = array('','января','февраля','марта','апреля','мая','июня','июля','августа','сентября','октября','ноября','декабря');
+$dayow = array('вс', 'пн', 'вт', 'ср', 'чт', 'пт', 'сб');
+$month = array('', 'января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря');
 //Parameters
-if(!isset($city)){
-	$city = 23;
-	if (is_numeric($_GET["city"])) $city=$_GET["city"];
-}
+$city = intval($_GET["city"]) ? intval($_GET["city"]) : 23;
 $uid = 'noname';
 $xml =  simplexml_load_file('http://xml.weather.co.ua/1.2/forecast/' . $city . '?dayf=5&lang=ru&userid=' . $uid);
 //Output
@@ -37,31 +35,35 @@ echo '<div style="float:left;width:150px;font:8pt Arial;">Ветер</div>';
 echo '<div style="float:left;width:150px;font:8pt Arial;">Влажность</div>';
 //table
 $last = -1;
-foreach ($xml->forecast->day as $item) {
+foreach ($xml->forecast->day as $item)
+{
 	$date = strtotime($item->attributes()->date);
-	if ($last != strftime('%d',$date)){
+	if ($last != strftime('%d', $date))
+	{
 		if ($last!=-1)
 			echo '<div style="clear:left;float:left;height:10px"> </div>';
-		echo ((strftime('%w', $date) == 0)||(strftime('%w', $date)==6)) ? '<div style="color:#f30">' : '';
+		echo ((strftime('%w', $date) == 0)||(strftime('%w', $date) == 6)) ? '<div style="color:#f30">' : '';
 		echo '<div style="position:relative;left:8px;clear:left;float:left;width:30px;">' . $dayow[strftime('%w', $date)] . '</div>';
 		echo '<div style="position:relative;top:-4px;float:left;width:70px;"><div style="font:28pt Arial">' . strftime('%d',$date) . '</div>';
 		echo '<div style="position:relative;top:-8px">' . $month[(int) strftime('%m',$date)] . '</div></div>';
-		echo ((strftime('%w', $date) == 0)||(strftime('%w', $date)==6)) ? '</div>' : '';
-	}else{
+		echo ((strftime('%w', $date) == 0)||(strftime('%w', $date) == 6)) ? '</div>' : '';
+	}
+	else
+	{
 		echo '<div style="clear:left;float:left;width:100px;"> </div>';
 	}
-	$last = strftime('%d',$date);
+	$last = strftime('%d', $date);
 	echo '<div style="float:left;background:#f0f0ec;height:62px">';
 	echo '<div style="float:left;font:10pt Arial;color:#666;position:relative;left:10px;width:150px">';
 	switch ($item->attributes()->hour){
-		case 3: echo 'утром';break;
-		case 9: echo 'днем';break;
-		case 15: echo 'вечером';break;
-		case 21: echo 'ночью';break;
+		case 3: echo 'утром'; break;
+		case 9: echo 'днем'; break;
+		case 15: echo 'вечером'; break;
+		case 21: echo 'ночью'; break;
 	}
 	echo '<div style="font:14pt Arial;color:#000;">' . $item->t->min . '…' . $item->t->max . ' °C</div>';
 	echo '</div>';
-	$wpic = explode('.',$item->pict);
+	$wpic = explode('.', $item->pict);
 	echo '<div style="float:left;width:100px;"><img src="http://www.weather.ua/images/wclipart/wst2/' . $wpic[0] . '.png" alt=""></div> ';
 	echo '<div style="float:left;position:relative;top:20px;left:10px;width:180px;">' . $item->p->min . '…' . $item->p->max . ' мм рт. ст.</div>' ;
 	echo '<div style="float:left;position:relative;top:20px;left:10px;width:150px">' . get_direction($item->wind->rumb) . ', ' . $item->wind->min . '…' . $item->wind->max . ' м/с</div>';
